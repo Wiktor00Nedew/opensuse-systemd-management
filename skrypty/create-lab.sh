1672c638e16b8fc409ad5e9d9f27ce442fa32b94
Wiktor00Nedew <wiktor.a.n.20@gmail.com>
2023-02-08 09:21:09 +0000

    switched to env

--- skrypty/create-lab.sh
+++ skrypty/create-lab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if mkdir $DOCKER_DIR/$1  > /dev/null 2>&1 && cp -r /sftp/docker/build $DOCKER_DIR/$1/build  > /dev/null 2>&1 && cp /sftp/docker/$1.yml $DOCKER_DIR/$1/docker-compose.yml > /dev/null 2>&1 && cd $DOCKER_DIR/$1 > /dev/null 2>&1 && docker network create $1  > /dev/null 2>&1 && docker-compose up -d > /dev/null 2>&1 ; then
+if mkdir $DOCKER_DIR/$1  > /dev/null 2>&1 && cp -r $SOURCE_DIR/build $DOCKER_DIR/$1/build  > /dev/null 2>&1 && cp $SOURCE_DIR/$1.yml $DOCKER_DIR/$1/docker-compose.yml > /dev/null 2>&1 && cd $DOCKER_DIR/$1 > /dev/null 2>&1 && docker network create $1  > /dev/null 2>&1 && docker-compose up -d > /dev/null 2>&1 ; then
     echo 1
 else
     echo 0
